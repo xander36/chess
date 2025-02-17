2ceba76835ddba28bdf7802720e8bbf22bb8cc18
xander36 <alexander.r.curtis36@gmail.com>
2025-02-17 09:56:18 -0700

    DAOs defined and endpoints started

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,10 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (req, res) -> {
+            res.type("application/json");
+            return "{\"message\": \"Hello, World!\"}";
+        });
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
