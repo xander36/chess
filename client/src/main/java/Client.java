5d2da860c4c7243f15265dfe37645d764d38050f
xander36 <alexander.r.curtis36@gmail.com>
2025-03-24 18:15:03 -0600

    code quality

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -9,7 +9,6 @@ import request.*;
 import result.*;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 import static ui.EscapeSequences.*;
 
@@ -35,161 +34,191 @@ public class Client {
         } else {
             if (status.equals("LOGGED_OUT")) {
                 if (input.startsWith("register")) {
-                    String[] parts = input.split(" ");
-                    if (parts.length != 4) {
-                        return "Please provide a username, password, and email";
-                    } else {
-                        RegisterRequest req = new RegisterRequest(parts[1], parts[2], parts[3]);
-                        try {
-                            RegisterResult res = serverFacade.register(req);
-                            status = "LOGGED_IN";
-                            username = res.username();
-                            authToken = res.authToken();
-
-                            recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
-
-                            return "Registered user " + res.username() + " and logged in";
-                        } catch (ServerFacadeException e) {
-                            if (e.toString().contains("taken")){
-                                return "That username is taken, try another";
-                            }
-                        }
-
-                    }
+                    return doRegister(input);
                 } else if (input.startsWith("login")) {
-                    String[] parts = input.split(" ");
-                    if (parts.length != 3) {
-                        return "Please provide a username and password";
-                    } else {
-                        LoginRequest req = new LoginRequest(parts[1], parts[2]);
-                        try{
-                            LoginResult res = serverFacade.login(req);
-
-                            status = "LOGGED_IN";
-                            username = res.username();
-                            authToken = res.authToken();
-                            recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
-                            return "Logged in user " + res.username();
-                        } catch(ServerFacadeException e) {
-                            if (e.toString().contains("unauthorized")){
-                                return "That combination of username and password is unrecognized, try logging in again";
-                            }
-                            System.out.println(e.toString());
-                        }
-
-                    }
+                    return doLogin(input);
                 }
             } else if (status.equals("LOGGED_IN")) {
                 if (input.startsWith("create")) {
-                    String[] parts = input.split(" ");
-                    if (parts.length != 2) {
-                        return "Invalid, please provide a name for the game to create";
-                    } else {
-
-                        MakeGameRequest req = new MakeGameRequest(authToken, parts[1]);
-                        MakeGameResult res = serverFacade.makeGame(req);
-
-                        recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
-
-                        return "Game called " + parts[1] + " has been started";
-                    }
+                    return doCreate(input);
                 } else if (input.startsWith("list")) {
-                    ListRequest req = new ListRequest(authToken);
-                    ListResult res = serverFacade.listGames(req);
-
-                    recentGameListing = res.games();
-
-                    StringBuilder outMsg = new StringBuilder();
-
-                    outMsg.append("All current games:\n");
-                    for (GameData game : res.games()){
-
-
-                        int gameNum = game.gameID();
-                        String whitePlayer = game.whiteUsername();
-                        String blackPlayer = game.blackUsername();
-                        String gameName = game.gameName();
-
-
-                        outMsg.append(gameNum);
-                        outMsg.append("- ");
-                        outMsg.append(gameName);
-                        outMsg.append(": ");
-                        if (whitePlayer.equals("null")){
-                            outMsg.append("<waiting for player>");
-                        } else{
-                            outMsg.append(whitePlayer);
-                        }
-                        outMsg.append(" (WHITE) vs ");
-                        if (blackPlayer.equals("null")){
-                            outMsg.append("<waiting for player>");
-                        } else{
-                            outMsg.append(blackPlayer);
-                        }
-                        outMsg.append(" (BLACK)\n");
-                    }
-
-                    return outMsg.toString().trim();
-
+                    return doList();
                 } else if (input.startsWith("join")) {
-                    String[] parts = input.split(" ");
-                    if (parts.length != 3) {
-                        return "Invalid arguments";
-                    } else if (!(parts[2].equals("WHITE") || parts[2].equals("BLACK"))){
-                        return "Invalid team name. try \"WHITE\" or \"BLACK\"";
-                    } else {
-                        int gameID = -1;
-                        try{
-                            gameID = Integer.parseInt(parts[1]);
-                        } catch (NumberFormatException e){
-                            return "Game ID is not a number... did you put the name instead?";
-                        }
-
-                        JoinGameRequest req = new JoinGameRequest(authToken, parts[2], gameID);
-                        try{
-                            serverFacade.joinGame(req);
-                            game = getGameWithID(Integer.parseInt(parts[1]));
-                            if (game == null){
-                                return "No game with that ID";
-                            }
-
-                            return "User " + username + " has joined game #" + game.gameID() + " as the " + parts[2] + " player" + "\n" + getBoard(parts[2]);
-
-                        } catch (ServerFacadeException e) {
-                            if (e.toString().contains("taken")){
-                                return "That player spot is taken";
-                            } else if (e.toString().contains("bad")){
-                                return "No game with that ID, try again";
-                            }
-                        }
-
-                    }
+                    return doJoin(input);
                 } else if (input.startsWith("observe")) {
-                    String[] parts = input.split(" ");
-                    if (parts.length != 2) {
-                        return "Please provide a game ID to observe";
-                    } else {
-                        game = getGameWithID(Integer.parseInt(parts[1]));
-                        if (game == null){
-                            return "No game with that ID";
-                        }
-
-                        return "Lets observe game #" + parts[1] + "\n" + getBoard("WHITE");
-                    }
+                    return doObserve(input);
                 } else if (input.startsWith("logout")) {
-                    LogoutRequest req = new LogoutRequest(authToken);
-                    serverFacade.logout(req);
-
-                    status = "LOGGED_OUT";
-                    String outMsg = "Logged out successfully. So long, " + username + "!";
-                    username = null;
-                    authToken = null;
-
-                    return outMsg;
+                    return doLogout();
                 }
             }
         }
         return "Alright Curtis you forgot to implement something";
+    }
+
+    private String doLogout() {
+        LogoutRequest req = new LogoutRequest(authToken);
+        serverFacade.logout(req);
+
+        status = "LOGGED_OUT";
+        String outMsg = "Logged out successfully. So long, " + username + "!";
+        username = null;
+        authToken = null;
+
+        return outMsg;
+    }
+
+    private String doObserve(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 2) {
+            return "Please provide a game ID to observe";
+        } else {
+            game = getGameWithID(Integer.parseInt(parts[1]));
+            if (game == null){
+                return "No game with that ID";
+            }
+
+            return "Lets observe game #" + parts[1] + "\n" + getBoard("WHITE");
+        }
+    }
+
+    private String doJoin(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            return "Invalid arguments";
+        } else if (!(parts[2].equals("WHITE") || parts[2].equals("BLACK"))){
+            return "Invalid team name. try \"WHITE\" or \"BLACK\"";
+        } else {
+            int gameID = -1;
+            try{
+                gameID = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e){
+                return "Game ID is not a number... did you put the name instead?";
+            }
+
+            JoinGameRequest req = new JoinGameRequest(authToken, parts[2], gameID);
+            try{
+                serverFacade.joinGame(req);
+                game = getGameWithID(Integer.parseInt(parts[1]));
+                if (game == null){
+                    return "No game with that ID";
+                }
+
+                return "User " + username + " has joined game #" + game.gameID() + " as the " + parts[2] + " player" + "\n" + getBoard(parts[2]);
+
+            } catch (ServerFacadeException e) {
+                if (e.toString().contains("taken")){
+                    return "That player spot is taken";
+                } else if (e.toString().contains("bad")){
+                    return "No game with that ID, try again";
+                }
+            }
+
+        }
+        return null;
+    }
+
+    private String doList() {
+        ListRequest req = new ListRequest(authToken);
+        ListResult res = serverFacade.listGames(req);
+
+        recentGameListing = res.games();
+
+        StringBuilder outMsg = new StringBuilder();
+
+        outMsg.append("All current games:\n");
+        for (GameData game : res.games()){
+
+
+            int gameNum = game.gameID();
+            String whitePlayer = game.whiteUsername();
+            String blackPlayer = game.blackUsername();
+            String gameName = game.gameName();
+
+
+            outMsg.append(gameNum);
+            outMsg.append("- ");
+            outMsg.append(gameName);
+            outMsg.append(": ");
+            if (whitePlayer.equals("null")){
+                outMsg.append("<waiting for player>");
+            } else{
+                outMsg.append(whitePlayer);
+            }
+            outMsg.append(" (WHITE) vs ");
+            if (blackPlayer.equals("null")){
+                outMsg.append("<waiting for player>");
+            } else{
+                outMsg.append(blackPlayer);
+            }
+            outMsg.append(" (BLACK)\n");
+        }
+
+        return outMsg.toString().trim();
+    }
+
+    private String doCreate(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 2) {
+            return "Invalid, please provide a name for the game to create";
+        } else {
+
+            MakeGameRequest req = new MakeGameRequest(authToken, parts[1]);
+            MakeGameResult res = serverFacade.makeGame(req);
+
+            recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
+
+            return "Game called " + parts[1] + " has been started";
+        }
+    }
+
+    private String doLogin(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            return "Please provide a username and password";
+        } else {
+            LoginRequest req = new LoginRequest(parts[1], parts[2]);
+            try{
+                LoginResult res = serverFacade.login(req);
+
+                status = "LOGGED_IN";
+                username = res.username();
+                authToken = res.authToken();
+                recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
+                return "Logged in user " + res.username();
+            } catch(ServerFacadeException e) {
+                if (e.toString().contains("unauthorized")){
+                    return "That combination of username and password is unrecognized, try logging in again";
+                }
+                System.out.println(e.toString());
+            }
+
+        }
+        return null;
+    }
+
+    private String doRegister(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 4) {
+            return "Please provide a username, password, and email";
+        } else {
+            RegisterRequest req = new RegisterRequest(parts[1], parts[2], parts[3]);
+            try {
+                RegisterResult res = serverFacade.register(req);
+                status = "LOGGED_IN";
+                username = res.username();
+                authToken = res.authToken();
+
+                recentGameListing = serverFacade.listGames(new ListRequest(authToken)).games();
+
+                return "Registered user " + res.username() + " and logged in";
+            } catch (ServerFacadeException e) {
+                if (e.toString().contains("taken")){
+                    return "That username is taken, try another";
+                }
+            }
+
+        }
+        return null;
     }
 
     private String getBoard(String team){
@@ -197,31 +226,31 @@ public class Client {
         String gameString = game.game().toString();
 
         StringBuilder fancyString = new StringBuilder();
-        String LETTER_ROW;
-        String[] NUMBER_COLUMN;
+        String letterRow;
+        String[] numberColumn;
         if (team.equals("WHITE")){
-            LETTER_ROW = "    a  b  c  d  e  f  g  h    ";
-            NUMBER_COLUMN = new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "};
+            letterRow = "    a  b  c  d  e  f  g  h    ";
+            numberColumn = new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "};
 
         } else if (team.equals("BLACK")){
-            LETTER_ROW = "    h  g  f  e  d  c  b  a     ";
-            NUMBER_COLUMN = new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "};
+            letterRow = "    h  g  f  e  d  c  b  a     ";
+            numberColumn = new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "};
         } else{
             return "C'mon son";
         }
 
-        LETTER_ROW = SET_BG_COLOR_DARK_GREEN + SET_TEXT_COLOR_WHITE + LETTER_ROW;
+        letterRow = SET_BG_COLOR_DARK_GREEN + SET_TEXT_COLOR_WHITE + letterRow;
         for (int i = 0; i < 8; i++){
-            NUMBER_COLUMN[i] = SET_BG_COLOR_DARK_GREEN + SET_TEXT_COLOR_WHITE + NUMBER_COLUMN[i];
+            numberColumn[i] = SET_BG_COLOR_DARK_GREEN + SET_TEXT_COLOR_WHITE + numberColumn[i];
         }
 
 
-        fancyString.append(LETTER_ROW + RESET_BG_COLOR + "\n");
+        fancyString.append(letterRow + RESET_BG_COLOR + "\n");
 
         ChessBoard board = new ChessBoard(gameString.substring(6));
 
         for (int i = 1; i < 9; i++){
-            fancyString.append(NUMBER_COLUMN[i-1]);
+            fancyString.append(numberColumn[i-1]);
             for (int j = 1; j < 9; j++){
                 if ((i+j) % 2 == 0){
                     fancyString.append(SET_BG_COLOR_WHITE);
@@ -240,9 +269,9 @@ public class Client {
                     fancyString.append(" " + piece.toString() + " ");
                 }
             }
-            fancyString.append(NUMBER_COLUMN[i-1] + RESET_BG_COLOR + "\n");
+            fancyString.append(numberColumn[i-1] + RESET_BG_COLOR + "\n");
         }
-        fancyString.append(LETTER_ROW + RESET_BG_COLOR);
+        fancyString.append(letterRow + RESET_BG_COLOR);
 
         return fancyString.toString();
     }
--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -223,7 +223,6 @@ public class ServiceTests {
             Assertions.fail();
         }reset();
 
-
     }
 
     @Test
